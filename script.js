5424a1435825c1fc5be195b5eef784209788a09c
therealbarnab <maxime.delvaux@outlook.fr>
2020-09-28 12:38:18 +0200

    Set up canvas

---
+++ script.js
@@ -0,0 +1,36 @@
+window.onload = function()
+{
+	
+	var canvas;
+	var ctx;
+	var delay = 1000;
+	var xCoord = 0;
+	var yCoord = 0;
+
+	init();
+
+
+	function init()
+	{
+
+		canvas = document.createElement('canvas');
+		canvas.width = 900;
+		canvas.height = 600;
+		canvas.style.border = "2px solid #444";
+		document.body.appendChild(canvas);
+		ctx = canvas.getContext('2d');
+		refreshCanvas();
+	}
+
+	function refreshCanvas()
+	{
+		
+		xCoord += 2;
+		yCoord += 2;
+		ctx.clearRect(0,0,canvas.width, canvas.height);
+		ctx.fillStyle = "#444";
+		ctx.fillRect(xCoord, yCoord, 100, 50);
+		setTimeout(refreshCanvas,delay)
+	}
+
+}
