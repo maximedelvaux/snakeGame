25503670c754cb5db5044c9ad893b9f736066f61
therealbarnab <maxime.delvaux@outlook.fr>
2020-09-28 13:40:08 +0200

    Drawing the snake and making it able to move

--- script.js
+++ script.js
@@ -1,36 +1,134 @@
 window.onload = function()
 {
 	
-	var canvas;
+	var canvasWidth = 900;
+	var canvasHeight = 600;
+	var blockSize = 30;
 	var ctx;
-	var delay = 1000;
-	var xCoord = 0;
-	var yCoord = 0;
+	var delay = 90;
+	var snakee;
 
 	init();
 
 
+	//Intialise the game 
 	function init()
 	{
-
-		canvas = document.createElement('canvas');
-		canvas.width = 900;
-		canvas.height = 600;
+		var canvas = document.createElement('canvas');
+		canvas.width = canvasWidth;
+		canvas.height = canvasHeight;
 		canvas.style.border = "2px solid #444";
 		document.body.appendChild(canvas);
 		ctx = canvas.getContext('2d');
+		snakee = new Snake([[6,4], [5,4], [4,4]], "right");
 		refreshCanvas();
 	}
 
 	function refreshCanvas()
 	{
-		
-		xCoord += 2;
-		yCoord += 2;
-		ctx.clearRect(0,0,canvas.width, canvas.height);
-		ctx.fillStyle = "#444";
-		ctx.fillRect(xCoord, yCoord, 100, 50);
-		setTimeout(refreshCanvas,delay)
+			
+		ctx.clearRect(0,0,canvasWidth, canvasHeight);
+		snakee.advance(); 
+		snakee.draw();
+		setTimeout(refreshCanvas,delay);
 	}
+
+	function drawBlock(ctx, position)
+	{
+		var x = position[0] * blockSize;
+		var y = position[1] * blockSize;
+		ctx.fillRect(x, y , blockSize, blockSize);
+	}
+
+	function Snake(body, direction)
+	{
+		this.body = body;
+		this.direction = direction;
+		this.draw = function()
+		{
+			ctx.save();
+			ctx.fillStyle = "#444";
+
+			for(var i=0; i < this.body.length; i++)
+			{
+				drawBlock(ctx,this.body[i]);
+			}
+			ctx.restore();
+		}; 
+
+		this.advance = function() 
+		{
+			var nextPosition = this.body[0].slice();
+			switch(this.direction)
+			{
+				case "left" :
+					nextPosition[0] -= 1;
+				break;
+				case "right" :
+					nextPosition[0] += 1;
+				break;
+				case "up" :
+					nextPosition[1] -= 1;
+				break;
+				case "down" :
+					nextPosition[1] += 1;
+				break;
+			default:
+				throw("Invalid direction");
+			}
+			this.body.unshift(nextPosition);
+			this.body.pop();
+		};
+		this.setDirection = function(newDirection)
+		{
+			var allowedDirections;
+			switch(this.direction)
+			{
+				case "left" :
+				case "right" :
+					allowedDirections = ["up", "down"];
+				break;
+				case "up" :
+				case "down" :
+					allowedDirections = ["left", "right"];
+				break;	
+			default:
+				throw("Invalid direction");
+			}
+			if (allowedDirections.indexOf(newDirection) > -1)
+			{
+				this.direction = newDirection;
+			}
+
+		}
+
+	}
+document.onkeydown = function handleKeyDown(e)
+{
+	var key = e.keyCode;
+	var newDirection;
+	switch(key)
+	{
+		case 37 :
+			newDirection = "left";
+		break;
+		case 38 :
+			newDirection = "up";
+		break;
+		case 39 :
+			newDirection = "right";
+		break;
+		case 40 :
+			newDirection = "down";
+		break; 
+	default:
+		return;	
+	}
+
+	snakee.setDirection(newDirection);
+
+}
+
+
 
 }
